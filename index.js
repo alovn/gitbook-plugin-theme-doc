01abf16ce20d1816e64767d73b44d8d47f89d9fd
alovn <alovn@live.com>
2021-03-09 05:34:52 +0800

    fix: logo and favicon path

--- index.js
+++ index.js
@@ -13,39 +13,39 @@ module.exports = {
     },
     hooks: {
         finish: function () {
-            var configOption = this.config.get('pluginsConfig')['theme-doc'];
-            var output = configOption ? (configOption.output || '_book') : '_book';
-            var pathFile;
+            // var configOption = this.config.get('pluginsConfig')['theme-doc'];
+            // var output = configOption ? (configOption.output || '_book') : '_book';
+            // var pathFile;
 
-            // favicon
-            pathFile = configOption && configOption.favicon;
-            if (pathFile) {
-                var faviconPath = path.join(process.cwd(), pathFile);
-                var gitbookFaviconPath = path.join(process.cwd(), output, 'gitbook', 'images', 'favicon.ico');
-                if (fs.existsSync(faviconPath)) {
-                    fs.writeFileSync(gitbookFaviconPath, fs.readFileSync(faviconPath));
-                }
-            }
+            // // favicon
+            // pathFile = configOption && configOption.favicon;
+            // if (pathFile) {
+            //     var faviconPath = path.join(process.cwd(), pathFile);
+            //     var gitbookFaviconPath = path.join(process.cwd(), output, 'gitbook', 'images', 'favicon.ico');
+            //     if (fs.existsSync(faviconPath)) {
+            //         fs.writeFileSync(gitbookFaviconPath, fs.readFileSync(faviconPath));
+            //     }
+            // }
 
-            // appleTouchIconPrecomposed152
-            pathFile = configOption && configOption.appleTouchIconPrecomposed152;
-            if (pathFile) {
-                var appleTouchIconPrecomposed152 = path.join(process.cwd(), pathFile);
-                var gitbookAppleTouchPath = path.join(process.cwd(), output, 'gitbook', 'images', 'apple-touch-icon-precomposed-152.png');
-                if (fs.existsSync(appleTouchIconPrecomposed152)) {
-                    fs.writeFileSync(gitbookAppleTouchPath, fs.readFileSync(appleTouchIconPrecomposed152));
-                }
-            }
+            // // appleTouchIconPrecomposed152
+            // pathFile = configOption && configOption.appleTouchIconPrecomposed152;
+            // if (pathFile) {
+            //     var appleTouchIconPrecomposed152 = path.join(process.cwd(), pathFile);
+            //     var gitbookAppleTouchPath = path.join(process.cwd(), output, 'gitbook', 'images', 'apple-touch-icon-precomposed-152.png');
+            //     if (fs.existsSync(appleTouchIconPrecomposed152)) {
+            //         fs.writeFileSync(gitbookAppleTouchPath, fs.readFileSync(appleTouchIconPrecomposed152));
+            //     }
+            // }
 
-            //logo
-            pathFile = configOption && configOption.logo;
-            if(pathFile){
-                var logoPath = path.join(process.cwd(), pathFile);
-                var pluginLogoPath = path.join(process.cwd(), output, 'gitbook','gitbook-plugin-theme-doc',"logo.png");
-                if (fs.existsSync(logoPath)) {
-                    fs.writeFileSync(pluginLogoPath, fs.readFileSync(logoPath));
-                }
-            }
+            // //logo
+            // pathFile = configOption && configOption.logo;
+            // if(pathFile){
+            //     var logoPath = path.join(process.cwd(), pathFile);
+            //     var pluginLogoPath = path.join(process.cwd(), output, 'gitbook','gitbook-plugin-theme-doc',"logo.png");
+            //     if (fs.existsSync(logoPath)) {
+            //         fs.writeFileSync(pluginLogoPath, fs.readFileSync(logoPath));
+            //     }
+            // }
         }
     }
 };
